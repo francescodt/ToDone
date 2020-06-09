be5675a0ba0bc90fb04fb6eb3890ed6b43444ad1
Francesco <francescodtroiano@gmail.com>
2020-06-08 20:33:48 -0500

    User identity

---
+++ ToDone/ToDone/Models/ToDoUser.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDone.Models
+{
+    public class ToDoUser : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        //no I won't
+    }
+}
