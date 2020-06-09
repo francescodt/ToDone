578895965a80f62d18a77af81b24c861c61553fe
Francesco <francescodtroiano@gmail.com>
2020-06-08 20:10:24 -0500

    contructor for db context

--- ToDone/ToDone/Data/ToDoListDbContext.cs
+++ ToDone/ToDone/Data/ToDoListDbContext.cs
@@ -1,11 +1,17 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace ToDone.Data
 {
-    public class ToDoListDbContext
+    public class ToDoListDbContext : DbContext
     {
+
+        public ToDoListDbContext (DbContextOptions<ToDoListDbContext> options) : base(options)
+        {
+
+        }
     }
 }
