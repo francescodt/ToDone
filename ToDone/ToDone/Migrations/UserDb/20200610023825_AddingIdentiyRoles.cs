ea0362743dfe4477c7997c3a8b14b6286cd63e0b
Francesco <francescodtroiano@gmail.com>
2020-06-09 21:41:26 -0500

    Concurrency stamp, identity roles

---
+++ ToDone/ToDone/Migrations/UserDb/20200610023825_AddingIdentiyRoles.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ToDone.Migrations.UserDb
+{
+    public partial class AddingIdentiyRoles : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "admin", "3231ab94-86fd-4b02-8046-548edea028cb", "Administrator", null });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "moderator", "d6d70432-9582-4aa7-ae8d-ed64c69f1add", "Moderator", null });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "admin");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "moderator");
+        }
+    }
+}
